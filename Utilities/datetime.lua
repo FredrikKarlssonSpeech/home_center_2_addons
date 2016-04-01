c7a41ecab5cfb9fada393377df8553f2e5f20919
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2016-04-01 10:31:25 +0200

    Documentation for functions in the datetime and sourcetrigger modules.

--- Utilities/datetime.lua
+++ Utilities/datetime.lua
@@ -1,16 +1,21 @@
---[[
-%% properties
-%% events
-%% globals
---]]
+--- A module that fascilitates specification of times when Fibaro lua scenes should run.
+--
 
 
---- A function that creates a os.date table from the time of sunset the same day.
+
+--- A function that creates a @{os.date} table from the time of sunset the same day.
 -- Provided that the function is not called exactly at midnight, the function will return a table that mathces the output of an os.date("*t")
 -- call made exactly the minute corresponding to the sunset hour.
--- @param time A text representation (e.g. "08:10") of the time of today to concert to a os.date date table. Allowed formats are "HH", "HH:MM" or "HH:MM:SS". "HH" is a short form for "HH:00" and "HH:MM" is a short for "HH:MM:00".
+-- @param time A text representation (e.g. "08:10") of the time of today to concert to a @{os.date} date table. Allowed formats are "HH", "HH:MM" or "HH:MM:SS". "HH" is a short form for "HH:00" and "HH:MM" is a short for "HH:MM:00".
 -- @return a table with year, month,day, hour min, sec and isdst fields.
 -- @see os.date
+-- @usage
+-- print(tostring(timestringToTable("08:10")))
+-- -- Will return 'true' when between 08:10  and 08:59
+-- print(tostring(timestringToTable("08")))
+-- -- Will return 'true' the entire hour
+-- print(tostring(timestringToTable("08:10:10")))
+-- -- Will return 'true' exactly at the indicated second
 
 function timestringToTable (time)
     local dateTable = os.date("*t");
@@ -47,8 +52,8 @@ end
 --
 -- A call isTime("08:10",45,10) will return 'true' from 08:54:50 to 08:55:10, and 'false' earlier or later than this.
 -- @param timeString The textual specification of the time to test against (e.g. "08:10").
--- @param offsetMinues The number of minutes to add to the 'timeString' time.
--- @param secondsWindo The size of the time window (in secons) in which the function will return 'true'. A zero (0) will cause the function to return 'true' only at 08:10:00 (and not 08:10:01) when called as isTime("08:10",0,0) so calls like that should be avoided.
+-- @param offsetMinutes The number of minutes to add to the 'timeString' time.
+-- @param secondsWindow The size of the time window (in secons) in which the function will return 'true'. A zero (0) will cause the function to return 'true' only at 08:10:00 (and not 08:10:01) when called as isTime("08:10",0,0) so calls like that should be avoided.
 -- @return A boolean (true or false) indicating whether the current time is within the specificed time range.
 
 function isTime (timeString, offsetMinutes, secondsWindow)
@@ -83,7 +88,7 @@ end
 -- @param dayList A list of short names of weekdays.
 -- @return A boolean (true /false) indicating whether the short name of today is given in the list.
 
-function isDayofWeek (dayList)
+function isDayOfWeek (dayList)
     local today = os.date("%a",os.time());
 --    fibaro:debug(tostring(today));
     for i, v in ipairs(dayList) do
@@ -100,6 +105,7 @@ end
 
 function isWeekDay ()
     local today = os.date("%w",os.time());
+    -- Please note that this specification is 0-6 range, sunday=0
     return (not (today == 0 or today == 6));
 end
 
@@ -115,23 +121,78 @@ end
 
 
 
---- A function that lets you specify date and time through a table
--- The table should use only fields that are returned by a call to os.time, i.e. a subset of year, month, day, hour, min, sec, wday, yday, isdst.
--- @param dateTable A time specification. For instance, {day=6,hour=15} returns 'true' on Sundays at 3 pm, {year=2016,month=2, hour=9} will return 'true' every day in February 2016 at 9 am, and 'false' at any time where parts of specification is not met.
--- @return A boolean (true/false) indicating the complete match of the table against current time and date.
+--- A function that lets you specify date and time in a very flexible way through a table
+-- The table should use only fields that are returned by a call to @{os.time} (year, month, day, hour, min, sec), 'wday' (1-7 scale, Sunday is 1) or 'yday' (day within the year, Jan 1st is 1).
+-- @tparam table dateTable A table giving a time specification.  For instance, {day=6,hour=15} returns 'true' on Sundays at 3 pm, {year=2016,month=2, hour=9} will return 'true' every day in February 2016 at 9 am, and 'false' at any time where parts of specification is not met. In each field, a table may be given, in which case any one of the options given will be accepted.
+-- @treturn boolean A boolean (true/false) indicating the complete match of the table against current time and date.
 -- @see os.time
 -- @usage datetimeTableTrue({month=2,minute=2})
--- -- # will return true every other minute in february.
+-- -- # will return 'true' every other minute in february.
+-- datetimeTableTrue({wday=2,yday=2})
+-- -- # will return 'true' when tested on the January 2nd, if it is a Monday.
+-- datetimeTableTrue({month=1,day=14,hour=6, minute=30})
+-- -- Will return 'true' when I wake up on my birthday
+-- datetimeTableTrue({month=1,day={14,15,16},hour=6, minute={0,30}})
+-- -- Will return 'true' on the 14th,15th or 16th of January at 6:00 or 6:30.
 
 
 function datetimeTableTrue (dateTable)
     local nowTodayTable = os.date("*t");
+    local scorekeeper = false;
     for k,v in pairs(dateTable) do
-        if not (nowTodayTable[k] == dateTable[k]) then
-            return(false);
+        -- Here I intentionally disregard the boolean isdst value, as the dateTable time specification should always be "true" time
+        if type (v) == "number" then
+            if not (nowTodayTable[k] == dateTable[k]) then
+                return(false);
+            end
+        elseif type (v) == "table" then
+            -- Here the logic is different. We cannot return 'false' until we have checked all elements in the list.
+            for ki, vi in pairs(v) do
+                if (nowTodayTable[k] == v[ki]) then
+                    scorekeeper = true;
+                end
+            end
+            if not scorekeeper then
+                return(false)
+            end
+            scorekeeper = false;
+        else
+            if not debug == nil then
+                fibaro:debug("List of options in a field in the table should only contain numbers")
+            else
+                error("List of options in a field in the table should only contain numbers")
+            end
         end
     end
     return(true);
 end
 
 --print(tostring(datetimeTableTrue({year=2016,hour=9})))
+
+-- local tab = {a=1,b={1,2}};
+
+--- Function that simplifies running a scene on a timer and specify the delay needed.
+-- The basic structure of the function is that it takes a truth value indicating whether the scene should run.
+-- The idea is that this truth value should be the result of a series of time or source evaluations combined
+-- into one evaluation by 'and' and 'or' joiners, possibly with nesting.
+-- The function @{myTimer} will then evaluate the function supplied as the second argument, if the first argument is evaluated to 'true'.
+-- After running the function constituting the scene, a delay may be imposed.
+-- @tparam boolean shouldRun A truth value. If evaluated to 'true', then the function will be run and the delay imposed.
+-- @tparam function functionToRun The function summarising the actions of the scene.
+-- @tparam number delaySeconds The number of seconds delay that should be imposed after having performed the scene.
+-- @usage
+-- function f () fibaro:call(12,"powerON") end
+-- -- A very simple scene
+-- myTimer ( notCurrentlyRunning() and isTime("08:10",0,20) and isDayOfWeek("Mon","Tues"), f, 2*60)
+-- -- This call will turn on switch 12 when tested from 08:09:40 to 08:10:20 on Mondays and Tuesdays
+-- -- and then sleep for 2 minutes in order to ensure that the scene is not run constantly,
+-- -- or more than once, as the 2 minutes delay combined with the call to @{notCurrentlyRunning} makes
+-- -- sure that it is not evaluated again within the 20 seconds time window allowed by the call to @{isTime}.
+
+function myTimer(shouldRun, functionToRun, delaySeconds )
+    if shouldRun then
+        functionToRun()
+        fibaro:sleep(delaySeconds*1000)
+    end
+end
+
---
+++ Utilities/sourcetriggers.lua
@@ -0,0 +1,55 @@
+--- A group of functions that makes testing for and handling of source triggers more managable
+-- The design of the functions allow for them to be called in a context where one would like to know if
+-- what caused the scene to trigger (i.e. in the context of an conditions testing of an if-statement ).
+-- If called outside of a context where truth value is not evaluated, then you may get more detailed
+-- on the trigger.
+--
+-- Since all values that are not 'false' or 'nil' are evaluated at true, this works.
+
+
+--- A function that tests whether the scene was started manually
+-- @treturn boolean A truth value (true /false)
+
+function startedManually ()
+    local startSource = fibaro:getSourceTrigger();
+    return( startSource["type"] == "other")
+end
+
+--- A function that tests whether the scene was started by a change in a global variable
+-- @treturn string If started by a global variable, the name of the variable is returned.
+-- @treturn boolean If started by something else, 'false' is returned.
+
+function startedByVariable ()
+    local startSource = fibaro:getSourceTrigger();
+    if startSource["type"] == "global" then
+        return (startSource["name"])
+    else
+        return false
+    end
+end
+
+--- A function that tests whether the scene was started by a change in the state or property of a device.
+-- @treturn table A table containting two fields:
+--  'deviceID', indicating the ID of the triggering device
+--  'propertyName' the property of the device that changed, causing the scene to trigger.
+-- @treturn boolean A truth value (true /false).
+function startedByDevice ()
+    local startSource = fibaro:getSourceTrigger();
+    -- startSource ={type="property",deviceID="11",propertyName="tet"}
+    if startSource["type"] == "property" then
+        return ({deviceID=startSource["deviceID"], propertyName=startSource["propertyName"]})
+    else
+        return false
+    end
+end
+
+--- A function that tests whether the scene is currently running.
+-- Please note that the testing is stated in the negative. What is tested is whether the scene is NOT
+-- running, as this is the most probable use case.
+-- @treturn boolean A truth value (true /false).
+
+
+function notCurrentlyRunning()
+    local sceneCount = fibaro:countScenes();
+    return (sceneCount == 1);
+end
