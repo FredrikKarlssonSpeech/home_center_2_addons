965067b2ab783f12f768c0fa4ef650b5375b0003
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2016-04-19 20:55:01 +0200

    First version of function

---
+++ Scenes/car_heater_off.lua
@@ -0,0 +1,17 @@
+--[[
+%% properties
+193 value
+%% events
+%% globals
+--]]
+
+
+local tempDeviceState0, deviceLastModification0 = fibaro:get(193, "value");
+fibaro:sleep(10800*1000)
+local temperature = tonumber(fibaro:getValue(3, "Temperature"));
+local tempDeviceState1, deviceLastModification1 = fibaro:get(193, "value");
+if( (deviceLastModification1 == deviceLastModification0) and (temperature > tonumber(-20) ) )  then
+
+    fibaro:call(193, "turnOff");
+
+end;
