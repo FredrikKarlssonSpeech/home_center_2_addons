d94f975700861ac714aeb848ada927bb79a7fb3e
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2016-09-17 09:20:03 +0200

    Updated versions of hte washing and dishwasher monitoring scenes.

---
+++ Scenes/dishwasher_alert.lua
@@ -0,0 +1,36 @@
+--[[
+%% properties
+279 power
+%% events
+%% globals
+--]]
+
+if (fibaro:countScenes() > 1) then
+  fibaro:abort();
+end
+
+local consumption = tonumber(fibaro:getValue(279, "power"));
+local prev = tostring(fibaro:getGlobal("Dishwasher"));
+  
+fibaro:debug("Dishwasher current consumption is"..consumption.. "W");
+
+-- Check state by current power consumption
+if (consumption < 50 ) then
+  fibaro:setGlobal("Dishwasher", "Not running");
+else
+  fibaro:setGlobal("Dishwasher", "Running");
+end;
+-- Check every 10 minutes
+fibaro:sleep(5*60*1000);
+local curr = tostring(fibaro:getGlobal("Dishwasher"));
+-- Check whether the state has changed!
+-- If it has, then do something about is, depending on the nature of the change!
+-- This provides a nice separation of logic. Could have easilly gone into the prev if clause.
+if ( curr ~= prev) then
+    if prev == "Running" then
+        -- In this case, the washing was running, but is not any more
+        fibaro:debug("Dishwasher done!");
+    else
+        fibaro:debug("Dishwasher started...");
+    end;
+end;
