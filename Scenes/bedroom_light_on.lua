04dfae72f89f5c4828231b8cf41db23d01f68bf5
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2016-09-25 21:27:52 +0200

    New working version. What was changed was the class of the "value". Also added some debuging.

---
+++ Scenes/bedroom_light_on.lua
@@ -0,0 +1,52 @@
+--[[
+%% properties
+251 value
+%% events
+%% globals
+--]]
+
+local takState = tonumber(fibaro:getValue(251, "value"));
+local sunsetHour = tostring(fibaro:getValue(1, "sunsetHour"));
+
+function timestringToTable (time)
+    local dateTable = os.date("*t");
+    -- Get an iterator that extracts date fields
+    local g =  string.gmatch(time, "%d+");
+
+    local hour = g() ;
+    local minute = g() or 0;
+    local second = g() or 0;
+    -- Insert sunset inforation istead
+    dateTable["hour"] = hour;
+    dateTable["min"] = minute;
+    dateTable["sec"] = second;
+    return(dateTable);
+end;
+
+function tableToEpochtime (t)
+    local now = os.date("*t");
+    local outTime = os.time{year=t.year or now.year, month=t.month or now.month,day=t.day or now.day,hour=t.hour or now.hour,min=t.min or now.min,sec=t.sec or now.sec,isdst=t.isdst or now.isdst};
+    return(outTime);
+end;
+
+function afterTime(time)
+    local timeEpoch = tableToEpochtime(timestringToTable(time));
+    local endOfDay = tableToEpochtime(timestringToTable("23:59:59"));
+    local now = os.time();
+    return( (now > timeEpoch) and (now <= endOfDay ));
+end;
+
+
+
+
+if (takState == 0) then
+    fibaro:debug(" 0 - Got state "..takState);
+    -- turned off
+    if (afterTime(sunsetHour)) then
+    
+        fibaro:call(346, "setValue", "30");
+    end;
+else
+    fibaro:debug(" 1 - Got state "..takState);
+    fibaro:call(346, "setValue", "0")  
+end;
