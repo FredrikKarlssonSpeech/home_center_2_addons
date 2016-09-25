99fcbeec51eed1a6fd7685c9b9dfd1640ec8e6f8
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2016-09-25 08:16:21 +0200

    Initial version of the startLevelChangeWithDur emulator. No stoping due to device change implemented.

---
+++ Utilities/lighting_utils.lua
@@ -0,0 +1,27 @@
+
+
+
+function startLevelChangeWithDur(dimmerID,startLevel,stopLevel,durationSeconds,delayBetweenSteps)
+    local currentLevel = startLevel;
+    local timeStepSeconds = delayBetweenSteps or 2;
+    local deltaPerSecond = (stopLevel - startLevel) / durationSeconds *timeStepSeconds; 
+    local newLevel = currentLevel + deltaPerSecond;
+    local currentLevelFromDevice = 0;
+
+    fibaro:debug("STARTING light increase");
+    repeat
+        fibaro:debug("Setting level value ".. newLevel);
+        fibaro:call(dimmerID,"setValue",newLevel);
+        currentLevel = newLevel;
+        -- wait for timeStepSeconds seconds
+        fibaro:sleep(timeStepSeconds* 1000);
+        -- check actual setting of the device now
+        currentLevelFromDevice = tonumber(fibaro:getValue(dimmerID, "value"));
+        newLevel = currentLevel + deltaPerSecond;
+        -- continue only if the level to be set is smaller or equal to the stop level
+        -- AND the level has not been manually decreased in the waiting period
+    until ( newLevel > stopLevel )
+  --or ( currentLevelFromDevice < currentLevel );
+
+    fibaro:debug("DONE light increase");
+end;
