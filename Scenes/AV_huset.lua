aeec5e255b8677e283d189a58ba4701022778d38
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2017-02-03 10:19:00 +0100

    Version bump

--- Scenes/AV_huset.lua
+++ Scenes/AV_huset.lua
@@ -6,6 +6,9 @@
 
 fibaro:startScene(98) -- Fönsterbelysningen
 
+fibaro:startScene(64) -- ACn
+fibaro:startScene(101) -- Alla innelampor
+
 fibaro:sleep(15*60) -- Vänta 15 minuter
 
 fibaro:startScene(82) -- Utebelysningen
