4112310dde735e7847f75aaaf6e1824f302771be
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2017-02-05 20:38:16 +0100

    Working version

--- Scenes/vardagsrummet_movement_lux_lights_autoON_OFF.lua
+++ Scenes/vardagsrummet_movement_lux_lights_autoON_OFF.lua
@@ -5,9 +5,6 @@
 %% globals
 --]]
 
--- SETUP
-
-local autoOffTime = 20 * 60; -- 20 minutes  delay
 
 -- Table of id, thershold pairs
 local lTab = {}
@@ -25,6 +22,54 @@ end;
 local currentLux = tonumber(fibaro:getValue(478, "value")); -- ljusnivå
 -- get movement time
 local lastBreach = tonumber(fibaro:getValue(476, "lastBreached"));
+
+function timeIsInRange (startTimeString, endTimeString)
+    local startTimeTable = timestringToTable(startTimeString);
+    local endTimeTable = timestringToTable(endTimeString);
+    local startTimeEpoch = tableToEpochtime (startTimeTable);
+    local endTimeEpoch = tableToEpochtime (endTimeTable);
+    local newEndTimeTable = endTimeTable;
+    -- allow for end time being in the upcoming day, but before startTime then
+    if (endTimeEpoch < startTimeEpoch) then
+        endTimeEpoch = endTimeEpoch + 24*3600;  -- add 24 hours
+        -- Now, we make a new table object, to find out whether the DST status of the new end time is
+        newEndTimeTable = os.date("*t",endTimeEpoch);
+    end;
+        -- Now, we adjust for Daylight saving time effects
+    if (startTimeTable.isdst == false and newEndTimeTable.isdst == true) then
+        -- In this case, start time is Not summer, and end time is in summer time
+        -- which means that we are going from spring into summer
+        -- then advance the clock one more hour from the end time
+        endTimeEpoch = endTimeEpoch + 3600;
+    elseif (startTimeTable.isdst == true and newEndTimeTable.isdst == false) then
+        -- Here, we are coming into fall (from summer)
+        -- then remove one hour from the time end time
+         endTimeEpoch = endTimeEpoch - 3600;
+    end;
+    local now = os.time();
+    return ( (startTimeEpoch <= now ) and (endTimeEpoch >= now));
+end;
+
+function timestringToTable (time)
+    local dateTable = os.date("*t");
+    -- Get an iterator that extracts date fields
+    local g =  string.gmatch(time, "%d+");
+
+    local hour = g() ;
+    local minute = g() or 0;
+    local second = g() or 0;
+    -- Insert sunset inforation istead
+    dateTable["hour"] = hour;
+    dateTable["min"] = minute;
+    dateTable["sec"] = second;
+    return(dateTable);
+end;
+
+function tableToEpochtime (t)
+    local now = os.date("*t");
+    local outTime = os.time{year=t.year or now.year, month=t.month or now.month,day=t.day or now.day,hour=t.hour or now.hour,min=t.min or now.min,sec=t.sec or now.sec,isdst=t.isdst or now.isdst};
+    return(outTime);
+end;
 
 function lightSelect(currentLuxLevel, conditionsTable)
     local currentLux = tonumber(currentLuxLevel);
@@ -59,6 +104,13 @@ for i, id in pairs(selectedLights) do
   fibaro:call(tonumber(id),"turnOn")
   fibaro:debug("Turned on lamp with ID ".. id);
 end;
+
+-- SETUP
+
+local autoOffTime = 1 * 60; -- 20 minutes  delay
+if (timeIsInRange ("06:30", "23:00")) then
+  autoOffTime = 20 * 60;
+end
 
 -- now handle auto OFF
 fibaro:sleep(autoOffTime*1000);
