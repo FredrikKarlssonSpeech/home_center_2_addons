744d4ce251a88134d52b41535d196638442bb4e2
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2017-11-12 18:57:49 +0100

    Structure for setting time of day

--- Utilities/time.lua
+++ Utilities/time.lua
@@ -1,6 +1,30 @@
 --- Functions that manages the defined time of day.
 -- @section timeofday
 
+--- An example time of day scheme that may be used as a default
+-- or altered to fit the indivdual's need.
+DEFAULT_TIME_OF_DAY = {
+	{["days"]={2,3,4,5,6},["time"]="06:30",["tod"]="Morning"},
+	{["days"]={1,7},["time"]="06:30",["tod"]="Morning"},
+	{["days"]={2,3,4,5,6},["time"]="08:00",["tod"]="Day"},
+	{["days"]={1,7},["time"]="10:00",["tod"]="Day"},
+	{["days"]={1,2,3,4,5,6,7},["time"]="18:00",["tod"]="Evening"},
+	{["days"]={2,3,4,5,6},["time"]="23:30",["tod"]="Night"},
+	{["days"]={1,7},["time"]="23:59",["tod"]="Night"}
+};
+
+function setTimeOfDay(todStructure)
+	local todStr = todStructure or DEFAULT_TIME_OF_DAY;
+	local wday = os.date("*t")["wday"];
+	for k,str in pairs(todStr) do
+		if tableValueExists(str["days"],wday) and afterTime(str["time"],0) then
+			fibaro:debug("Setting TimeOfDay to '".. str["tod"] .. "'")
+			fibaro:getGlobalValue("TimeOfDay",str["tod"]);
+			return(true);
+		end;
+	end;
+	return(false);
+end;
 
 --- A function that checks whether it is daytime or not
 -- This function checks the "TimeOfDay" global variable, and expects that the answer is "Day" if true.
