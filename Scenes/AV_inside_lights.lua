23bd0a8bf80f7386fd94c6720e1973124c407e0e
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2017-12-05 20:14:59 +0100

    Updated IDs since migration

--- Scenes/AV_inside_lights.lua
+++ Scenes/AV_inside_lights.lua
@@ -4,30 +4,53 @@
 %% globals
 --]]
 
-
-fibaro:call(268, "turnOff") -- Släck dimmern nere
+-- Vardagsrummet nere
+fibaro:call(724, "turnOff") -- Släck dimmern vid soffan nere
 fibaro:call(407, "turnOff") -- Släck höger vägglampa
 fibaro:call(408, "turnOff") -- Släck vänster vägglampa
+fibaro:call(821, "turnOff") -- Släck lampan vid matbordet
+-- Se till att fläkten nere inte är igång också
+fibaro:call(494, "turnOff") -- Fläkten i vardagsrummet
 
-fibaro:call(214, "turnOff") -- Släck badrummet nere
-
+fibaro:call(619, "turnOff") -- Släck badrummet nere
+-- Tvättstugan
 fibaro:call(314, "turnOff") -- Släck taket tvättstugan nere
 fibaro:call(316, "turnOff") -- Släck lilla lampan tvättstugan
 
-fibaro:call(264, "turnOff") -- Släck Alex
+-- Köket
+fibaro:call(796, "turnOff") -- köksbordet
+fibaro:call(811, "turnOff") -- taklamporna
+fibaro:call(816, "turnOff") -- spotar köksbänk
+fibaro:call(806, "turnOff") -- spotar öppningen
 
-fibaro:call(251, "turnOff") -- Släck Vårt sovrum
+-- Alex rum
+fibaro:call(772, "turnOff") -- Släck Alex
+
+-- Vårt rum
+fibaro:call(728, "turnOff") -- Släck Vårt sovrum
+
+fibaro:call(646, "turnOff") -- lilla förrådet
 
 
+-- VArdagsrummet uppe
 fibaro:call(57, "turnOff") -- Släck taklampan uppe
-fibaro:call(581, "turnOff") -- Släck över soffan
+fibaro:call(780, "turnOff") -- Släck över soffan
 fibaro:call(508, "turnOff") -- Släck trappen
 
-fibaro:call(310, "turnOff") -- Släck Theos sovrum
+fibaro:call(593, "turnOff") -- Släck Theos sovrum
 
-fibaro:call(306, "turnOff") -- Släck Isas sovrum
+fibaro:call(597, "turnOff") -- Släck Isas sovrum
 
-fibaro:call(62, "turnOff") -- Släck badrummet uppe
+fibaro:call(659, "turnOff") -- Släck badrummet uppe 
 
--- Se till att fläkten nere inte är igång också
-fibaro:call(494, "turnOff") -- Fläkten i vardagsrummet
+fibaro:call(719, "turnOff") -- Belysning i skrivarförrådet
+
+-- Garaget
+
+fibaro:call(28, "turnOff") -- Taklampan
+fibaro:call(449, "turnOff") -- arbetsbänken
+fibaro:call(451, "turnOff") -- förrådet
+
+
+
+
