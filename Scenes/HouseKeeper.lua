ed3a4168d0986186c806b692db8324c551abe78e
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2017-12-26 20:37:13 +0100

    Added date and holiday parts

--- Scenes/HouseKeeper.lua
+++ Scenes/HouseKeeper.lua
@@ -11,13 +11,29 @@ DEFAULT_TIME_OF_DAY = {
 	{["days"]={1,7},["time"]="23:59",["tod"]="Night"}
 };
 
+THIS_YEARS_HOLIDAYS = {
+    {["name"]="Jul",["start"]="24/12",["end"]="26/12"},
+    {["name"]="Nyår",["start"]="31/12",["end"]="1/1"},
+    {["name"]="Trettondedag Jul",["start"]="6/1",["end"]="6/1"},
+    {["name"]="Påsk",["start"]="2018-03-30",["end"]="2018-04-02"},
+    {["name"]="Första Maj",["start"]="1/5",["end"]="1/5"},
+    {["name"]="Kristi himmelfärdsdag",["start"]="2018-05-10",["end"]="2018-05-10"},
+    {["name"]="Pingstdagen",["start"]="2018-05-20",["end"]="2018-05-20"},
+    {["name"]="Sveriges Nationaldag",["start"]="6/6",["end"]="6/6"},
+    {["name"]="Midsommar",["start"]="2018-06-23",["end"]="2018-06-23"},
+    {["name"]="Alla helgons dag",["start"]="2018-11-03",["end"]="2018-11-03"},
+}
+
 function MY_MINUTEWISE_CHECKS()
 	setPartsOfWeek();
+	setHolidayStates(THIS_YEARS_HOLIDAYS);
 	setTimeOfDay(DEFAULT_TIME_OF_DAY);
 	setSunState(30);
 	--checkLastMovement();
 	doHousekeeping();
 end; 
+
+---- USER EDITING NOT REQUIRED BEYOND THIS MARK ---- 
 
 -- RUNNING CODE
 
@@ -32,6 +48,91 @@ function waitUntilNextMinute()
     local currSec = os.time() % 60;
     local toWait = 60 - currSec;
     fibaro:sleep(toWait *1000);
+end;
+
+-- DATE AND HOLIDAY STUFF
+
+function setHolidayStates(holidayStructure)
+    local setValue =tostring(fibaro:getGlobalValue("PartOfWeekToday"));
+    if isHoliday(holidayStructure) and setValue ~= "Holiday" then
+        fibaro:setGlobal("PartOfWeekToday","Holiday");
+    end;
+    setValue =tostring(fibaro:getGlobalValue("PartOfWeekTomorrow"))
+    if isHolidayTomorrow(holidayStructure) and setValue ~= "Holiday" then
+        fibaro:setGlobal("PartOfWeekTomorrow","Holiday");
+    end;
+end;
+
+function isHoliday(holidayStructure)
+
+    for k,hstr in pairs(holidayStructure) do
+        if isBetweenDates(today(),hstr["start"],hstr["end"]) then
+            return(true);
+        end;
+    end;
+    return(false);
+end;
+
+function isHolidayTomorrow(holidayStructure)  
+    for k,hstr in pairs(holidayStructure) do
+        if isBetweenDates(tomorrow(),hstr["start"],hstr["end"]) then
+            return(true);
+        end;
+    end;
+    return(false);
+end;
+
+function parseDate (dateString,startOfDay)
+    -- Get an iterator that extracts date fields
+    local g =  string.gmatch(dateString, "%d+");
+
+    local first = g() ;
+    local second = g();
+    local third = g() or nil;
+    local dateTable = {};
+    if first == "" or second == "" then
+        return(nil);
+    end;
+    if third ~= nil then
+        -- we have a full date
+        dateTable["year"] = first;
+        dateTable["month"] = tostring(tonumber(second));
+        dateTable["day"] = tostring(tonumber(third));
+    else
+        dateTable["year"] = os.date("%Y");
+        dateTable["month"] = tostring(tonumber(second));
+        dateTable["day"] = tostring(tonumber(first));
+    end;
+    if startOfDay == true then
+        dateTable['hour'] = 0;
+        dateTable['sec'] = 0;
+        dateTable['min'] = 0;
+    else
+        dateTable['hour'] = 23;
+        dateTable['sec'] = 59;
+        dateTable['min'] = 59;
+    end;
+
+    return(dateTable);
+end;
+
+
+function isBetweenDates (date,startDateString, endDateString)
+    local startDateTable = parseDate(startDateString,true);
+    local endDateTable = parseDate(endDateString,false);
+    local startDateEpoch = tableToEpochtime (startDateTable);
+    local endDateEpoch = tableToEpochtime (endDateTable);
+
+    local now = tableToEpochtime(parseDate(date));
+    return ( (startDateEpoch <= now ) and (endDateEpoch >= now));
+end;
+
+function today()
+  return(os.date("%Y-%m-%d",tonumber(os.time())));
+end;
+
+function tomorrow()
+  return(os.date("%Y-%m-%d",tonumber(os.time()+24*3600)));
 end;
 
 --- DAY OF WEEK
